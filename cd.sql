b098939fa4497ec2e56cf1c9e75bf0f9ddd5c025
angellyne <angellyne.marques@escola.pr.gov.br>
2025-09-01 11:08:03 -0300

    segundo commit

--- cd.sql
+++ cd.sql
@@ -15,3 +15,54 @@ estoque varchar (255),
 id int primary key auto_increment,
 consumivel_id int (11)
 );
+create table venda(
+	loja varchar(255),
+    valor_total varchar(255),
+    valor_item varchar(255),
+    data_compra date,
+    forma_pagamento varchar(255),
+    estado varchar(255),
+    id int(11)
+);
+
+create table login(
+	user varchar(255),
+	passaword varchar(255),
+	id int(11),
+	login_id int(11),
+	responsavel_id int(11)
+);
+
+create table agenda(
+	data date,
+	horainicio varchar(255),
+    horafim varchar(255),
+    disponivel varchar(255),
+    id int(11)
+);
+
+create table loja(
+	nome varchar(255),
+	email varchar(255),
+	telefone int(11),
+	cnpj varchar(255),
+	endereco varchar(255),
+	id int(11),
+	pet_id int(11),
+	catalogo_id int(11)
+);
+
+create table perfil(
+	fotoUrl varchar(255),
+	id int(11)
+);
+create table pet( 
+nome varchar(255),
+idade int(11),
+raca varchar(255),
+porte varchar(255),
+sexo varchar(255),
+responsavel varchar(255),
+id int(11),
+perfil_id int(11)
+);
